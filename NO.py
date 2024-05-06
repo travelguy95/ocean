598cf14b1fd1d44d9a2e3a8ff653233bad9c0715
travelguy95 <131559065+travelguy95@users.noreply.github.com>
2024-05-05 19:42:37 -0700

    Update NO.py

--- NO.py
+++ NO.py
@@ -320,7 +320,7 @@ def load_darcy_pt(data_path,
     """
 
     #Load the training data
-    data = torch.load(Path(data_path).joinpath(f'training.pt').as_posix())
+    data = torch.load(Path(data_path).joinpath(f'480K_wet_dry_training.pt').as_posix())
 
     x_train = data['x'][0:n_train, :, :].unsqueeze(channel_dim).type(torch.float32).clone()
     y_train = data['y'][0:n_train, :, :].unsqueeze(channel_dim).clone()
@@ -330,7 +330,7 @@ def load_darcy_pt(data_path,
     test_resolutions.pop(idx)
     n_test = n_tests.pop(idx)
     test_batch_size = test_batch_sizes.pop(idx)
-    data = torch.load(Path(data_path).joinpath(f'testing.pt').as_posix())
+    data = torch.load(Path(data_path).joinpath(f'480K_wet_dry_testing.pt').as_posix())
     x_test = data['x'][:n_test, :, :].unsqueeze(channel_dim).type(torch.float32).clone()
     y_test = data['y'][:n_test, :, :].unsqueeze(channel_dim).clone()
     del data
@@ -372,7 +372,7 @@ def load_darcy_pt(data_path,
 
     for (res, n_test, test_batch_size) in zip(test_resolutions, n_tests, test_batch_sizes):
         print(f'Loading test db at resolution {res} with {n_test} samples and batch-size={test_batch_size}')
-        data = torch.load(Path(data_path).joinpath(f'testing.pt').as_posix())
+        data = torch.load(Path(data_path).joinpath(f'480K_wet_dry_testing.pt').as_posix())
         x_test = data['x'][:n_test, :, :].unsqueeze(channel_dim).type(torch.float32).clone()
         y_test = data['y'][:n_test, :, :].unsqueeze(channel_dim).clone()
         del data
